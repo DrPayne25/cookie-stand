08314c531293e3648f40a86c3975c8fd00aa775b
DrPayne25 <alex.payne1125@gmail.com>
2021-05-12 20:02:45 -0700

    added photos and tested out my first object

--- js/app.js
+++ js/app.js
@@ -1,3 +1,36 @@
+'use strict';
+console.log('Hello World');
+//gets the element
+const mycontainer = document.getElementById('container');
+
+//Proof of Life get in the habit of doing this more
+console.log(mycontainer);
+
+//1.Create Element
+let section = document.createElement('section');
+//2.give it content
+let ul = document.createElement('ul');
+//3. append to the DOM
+mycontainer.appendChild(section);
+//Do this its important to do proof of life as you go
+
+// create element
+let img = document.createElement('img');
+//Content for this part
+img.src = 'img/tokyo.jpg';
+img.alt = 'picture of tokyo';
+// append
+section.appendChild(img);
+//proof of life
+
+//create article
+let article = document.createElement('article');
+//give content
+let h3 = document.createElement('h3');
+//give text content
+//append to the dom
+section.appendChild(article);
+
 let seattle = {
   name: 'Seattle',
   min: 23,
@@ -14,8 +47,8 @@ let seattle = {
     //in the loop
     let custThisHour = this.getCustomersPerHour();
     let cookiesSoldThisHour = Math.round(custThisHour * this.avg);
-    this.cookiesPerHourArray.push(cookiesSoldThisHour);
-    console.log('I Am in the calcCookiesPerHour method ', 'custThisHour: ', custThisHour, 'cookiesSoldThisHour: ', cookiesSoldThisHour);
+    //this.cookiesPerHourArray.push(cookiesSoldThisHour);
+    console.log(`I Am in the calcCookiesPerHour method custThisHour: ${custThisHour} cookiesSoldThisHour: , ${cookiesSoldThisHour}`);
   },
   render: function(){
     this.calcCookiesPerHour();
