83febf4046fd6bf4f4eb346bff8d55c15739c029
DrPayne25 <alex.payne1125@gmail.com>
2021-05-12 20:35:34 -0700

    created my 5 shop objects

--- js/app.js
+++ js/app.js
@@ -1,59 +1,139 @@
-'use strict';
-console.log('Hello World');
-//gets the element
-const mycontainer = document.getElementById('container');
+// 'use strict';
+// console.log('Hello World');
+// //gets the element
+// const mycontainer = document.getElementById('container');
 
-//Proof of Life get in the habit of doing this more
-console.log(mycontainer);
+// //Proof of Life get in the habit of doing this more
+// console.log(mycontainer);
 
-//1.Create Element
-let section = document.createElement('section');
-//2.give it content
-let ul = document.createElement('ul');
-//3. append to the DOM
-mycontainer.appendChild(section);
-//Do this its important to do proof of life as you go
+// //1.Create Element
+// let section = document.createElement('section');
+// //2.give it content
+// let ul = document.createElement('ul');
+// //3. append to the DOM
+// mycontainer.appendChild(section);
+// //Do this its important to do proof of life as you go
 
-// create element
-let img = document.createElement('img');
-//Content for this part
-img.src = 'img/tokyo.jpg';
-img.alt = 'picture of tokyo';
-// append
-section.appendChild(img);
-//proof of life
+// // create element
+// let img = document.createElement('img');
+// //Content for this part
+// img.src = 'img/tokyo.jpg';
+// img.alt = 'picture of tokyo';
+// // append
+// section.appendChild(img);
+// //proof of life
 
-//create article
-let article = document.createElement('article');
-//give content
-let h3 = document.createElement('h3');
-//give text content
-//append to the dom
-section.appendChild(article);
+// //create article
+// let article = document.createElement('article');
+// //give content
+// let h3 = document.createElement('h3');
+// //give text content
+// h3.textContent = ''
+// //append to the dom
+// section.appendChild(article);
+
+
+
+
+
+
+
+
+
+
+
+
+
+const hoursOpen = ['6am','7am','8am','9am','10am','11am','12am','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
+const Stores = [seattle, tokyo, dubai, paris, lima];
 
 let seattle = {
   name: 'Seattle',
   min: 23,
   max: 65,
-  dailyTotal: 0,
   avgCookiesSoldEachHourArray: [],
   avg: 6.3,
+  //get a random number of customers per hour
   getCustomersPerHour: function (){
     return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
   },
+  //fill out the avgCookiesSold
   calcCookiesPerHour: function(){
-    // calculate cookies for all 14 hours
-    // write a for loop that uses the length of the hours array
-    //in the loop
-    let custThisHour = this.getCustomersPerHour();
-    let cookiesSoldThisHour = Math.round(custThisHour * this.avg);
-    //this.cookiesPerHourArray.push(cookiesSoldThisHour);
-    console.log(`I Am in the calcCookiesPerHour method custThisHour: ${custThisHour} cookiesSoldThisHour: , ${cookiesSoldThisHour}`);
+    for (let i = 0; i < hoursOpen.length; i++){
+      this.avgCookiesSoldEachHourArray.push(Math.round(this.avg + this.getCustomersPerHour()));
+    }
+    return this.avgCookiesSoldEachHourArray;
   },
-  render: function(){
-    this.calcCookiesPerHour();
-    console.log('this is the render method');
-  }
 };
-
-seattle.render();
+let tokyo = {
+  name: 'Tokyo',
+  min: 3,
+  max: 24,
+  avgCookiesSoldEachHourArray: [],
+  avg: 1.2,
+  //get a random number of customers per hour
+  getCustomersPerHour: function (){
+    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+  },
+  //fill out the avgCookiesSold
+  calcCookiesPerHour: function(){
+    for (let i = 0; i < hoursOpen.length; i++){
+      this.avgCookiesSoldEachHourArray.push(Math.round(this.avg + this.getCustomersPerHour()));
+    }
+    return this.avgCookiesSoldEachHourArray;
+  },
+};
+let dubai = {
+  name: 'Dubai',
+  min: 11,
+  max: 38,
+  avgCookiesSoldEachHourArray: [],
+  avg: 3.7,
+  //get a random number of customers per hour
+  getCustomersPerHour: function (){
+    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+  },
+  //fill out the avgCookiesSold
+  calcCookiesPerHour: function(){
+    for (let i = 0; i < hoursOpen.length; i++){
+      this.avgCookiesSoldEachHourArray.push(Math.round(this.avg + this.getCustomersPerHour()));
+    }
+    return this.avgCookiesSoldEachHourArray;
+  },
+};
+let paris = {
+  name: 'Paris',
+  min: 20,
+  max: 38,
+  avgCookiesSoldEachHourArray: [],
+  avg: 2.3,
+  //get a random number of customers per hour
+  getCustomersPerHour: function (){
+    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+  },
+  //fill out the avgCookiesSold
+  calcCookiesPerHour: function(){
+    for (let i = 0; i < hoursOpen.length; i++){
+      this.avgCookiesSoldEachHourArray.push(Math.round(this.avg + this.getCustomersPerHour()));
+    }
+    return this.avgCookiesSoldEachHourArray;
+  },
+};
+let lima = {
+  name: 'lima',
+  min: 2,
+  max: 16,
+  avgCookiesSoldEachHourArray: [],
+  avg: 4.6,
+  //get a random number of customers per hour
+  getCustomersPerHour: function (){
+    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+  },
+  //fill out the avgCookiesSold
+  calcCookiesPerHour: function(){
+    for (let i = 0; i < hoursOpen.length; i++){
+      this.avgCookiesSoldEachHourArray.push(Math.round(this.avg + this.getCustomersPerHour()));
+    }
+    return this.avgCookiesSoldEachHourArray;
+  },
+};
